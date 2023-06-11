54f80dbd844b590bba3b57cc8ecd3d28b766e10e
Himanshu <92928990+himanshu060301@users.noreply.github.com>
2023-06-11 19:46:06 +0530

    Added my repo

---
+++ Booking App/admin/src/index.js
@@ -0,0 +1,34 @@
+<<<<<<< HEAD
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { DarkModeContextProvider } from "./context/darkModeContext";
+import { AuthContextProvider } from "./context/AuthContext";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthContextProvider>
+      <DarkModeContextProvider>
+        <App />
+      </DarkModeContextProvider>
+    </AuthContextProvider>
+    
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+>>>>>>> parent of b9226c6 (first commit)
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
